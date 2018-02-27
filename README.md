3e57790482e48d3debe76f041151d904076dd97f
Joseph Gormley <josephbgormley@gmail.com>
2018-02-27 16:00:52 -0500

    Working on Cracking the Coding Interview Questions with my LinkedList class. Fixed the indentation w/ nano.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Data-Structures
 Personal library of data structures, feel free to use. These will be written in Java. 
+
+Interweaving this with Cracking the Coding Interview Questions. 
+
+#1. Remove Dups: Write code to remove duplicates from an unsorted linked list. 
+    How would you solve this problem if a temporary buffer is not allowed?
--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -2,115 +2,115 @@
    Date: 12/6/17 */
 public class SinglyLinkedList<T extends Comparable<T>> {
 
-	/*************
-	 * MEMBER(S) *
-     *************/
-	Node<T> front; // Will represent first node in list. 
+  /*************
+   * MEMBER(S) *
+   *************/ 
+   Node<T> front; // Will represent first node in list. 
 
 
-	/******************
-	 * CONSTRUCTOR(S) *
-     ******************/
-	// Default constructor is fine for this class.
+  /******************
+   * CONSTRUCTOR(S) *
+   ******************/
+   // Default constructor is fine for this class.
 
 
-    /*************
-	 * METHOD(S) *
-     *************/
-	/* Inserts an item to the front of the list */
-	public void insertToFront(T data){
+  /*************
+   * METHOD(S) *
+   *************/
+   /* Inserts an item to the front of the list */
+   public void insertToFront(T data){
 
-		// Create Node for data. 
-		front = new Node<T>(data, front);
+      // Create Node for data. 
+      front = new Node<T>(data, front);
 
-	}
+   }
 
-	/* Inserts an item to the end of the list */
-	public void insertToEnd(T data){
+   /* Inserts an item to the end of the list */
+   public void insertToEnd(T data){
 
-		// Iterate to end of list.	
-		Node<T> endNode = front;
-		while(endNode.next != null){
-			endNode = endNode.next;
-		}
+      // Iterate to end of list.	
+      Node<T> endNode = front;
+      while(endNode.next != null){
+         endNode = endNode.next;
+      }
 
-		// Create Node for data and add to the list.
-		endNode.next = new Node<T>(data, null);
+      // Create Node for data and add to the list.
+      endNode.next = new Node<T>(data, null);
 
-	}
+   }
+ 
+   /* Deletes first occurrence of data in list */
+   public void delete(T data){
 
-	/* Deletes first occurrence of data in list */
-	public void delete(T data){
+      // Check to see if first item is first occurrence of data. 
+      if(front.data.compareTo(data) == 0){
+         if(front.next == null){
+            // Delete only item in list. 
+	    front = null;
+            return; 
+	 }
 
+	 front = front.next; 
+         return;
+      }  
 
-		// Check to see if first item is first occurrence of data. 
-		if(front.data.compareTo(data) == 0){
-			if(front.next == null){
-				// Delete only item in list. 
-				front = null;
-				return; 
-			}
+      Node<T> prev = front;
 
-			front = front.next; 
-			return;
-		}
+      // Search remaining of list for first occurrence of data.
+      for(Node<T> toDelete = front.next; toDelete != null; toDelete = toDelete.next){
+	 if(toDelete.data.compareTo(data) == 0){
+	    // Delete this node.
+	    prev.next = prev.next.next;
+	    return;
+	 }
 
-		Node<T> prev = front;
-		// Search remaining of list for first occurrence of data.
-		for(Node<T> toDelete = front.next; toDelete != null; toDelete = toDelete.next){
-			if(toDelete.data.compareTo(data) == 0){
-				// Delete this node.
-				prev.next = prev.next.next;
-				return;
-			}
+	 prev = toDelete;
+      }
 
-			prev = toDelete;
-
-		}
-
-	}
-
-	//TODO 
-	/* Deletes ALL occurences of data in list */
-	public void deleteAll(T data){
-
-
-	}
+   }
+        
+   // TODO  
+   /* Deletes ALL occurences of data in list */ 
+   public void deleteAll(T data){
 
 
-	/********************
-	 * HELPER METHOD(S) *
-     ********************/
-	/* Prints out the list */
-	public void printList(){
+   }
 
-		// Iterate list
-		for(Node<T> toPrint = front; toPrint != null; toPrint = toPrint.next){
-			System.out.println(toPrint.data);
-		}
 
-		return;
-	}
+  /********************
+   * HELPER METHOD(S) *
+   ********************/
+   /* Prints out the list */
+   public void printList(){
 
+      // Iterate list
+      for(Node<T> toPrint = front; toPrint != null; toPrint = toPrint.next){
+         System.out.println(toPrint.data);
+      }
+
+      return;
+   }
+
+// End of class. 
 }
  
 
 /* This class will store data and a pointer to the next data */
 class Node<T> {
 
-	/*******************
-	 * CLASS MEMBER(S) *
-     *******************/
-	T data; 
-	Node<T> next; 
+  /*******************
+   * CLASS MEMBER(S) *
+   *******************/
+   T data; 
+   Node<T> next; 
 
 
-	/******************
-	 * CONSTRUCTOR(S) *
-     ******************/
-	public Node(T data, Node<T> next){
-		this.data = data;
-		this.next = next;
-	}
-
+  /******************
+   * CONSTRUCTOR(S) *
+   ******************/
+   public Node(T data, Node<T> next){
+      this.data = data;
+      this.next = next;
+   }
+// End of class. 
 }

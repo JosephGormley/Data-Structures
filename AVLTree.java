23b8b5b6653282bcd77ba59a0617f0904e7dd2f1
Joseph Gormley <josephbgormley@gmail.com>
2018-03-14 01:23:22 -0400

    Cleaned up code for future documentation.

--- AVLTree.java
+++ AVLTree.java
@@ -1,17 +1,36 @@
 public class AVLTree {
 
+  /******************
+   * CLASS FIELD(S) *
+   ******************/
+
+  /************************
+   * CLASS CONSTRUCTOR(S) *  
+   ************************/
+
+  /*******************
+   * CLASS METHOD(S) *
+   *******************/
+
+   public void insert(int data){ }
+
    
 
-
-
-
-
+  /********************
+   * HELPER METHOD(S) *
+   ********************/
+   public void printInOrder(){ }
 
 }
 
 class TreeNode {
+ 
+  /******************
+   * CLASS FIELD(S) *
+   ******************/
 
-
-
+  /************************
+   * CLASS CONSTRUCTOR(S) *
+   ************************/
 
 }

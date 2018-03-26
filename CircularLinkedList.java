47777f971b9b7b41d2ea5e95e26acceb071ec2ea
Joseph Gormley <josephbgormley@gmail.com>
2018-03-26 18:21:08 -0400

    Added boilateplate code for CircularLinkedList data structure.

---
+++ CircularLinkedList.java
@@ -0,0 +1,43 @@
+/* Author: Joseph Gormle
+   Date: March 26, 2018 */
+
+public class DoublyLinkedList {
+
+
+  /******************
+   * CLASS FIELD(S) *
+   ******************/
+   int size;
+   Node front; 
+
+  /************************
+   * CLASS CONSTRUCTOR(S) *
+   ************************/ 
+   // Default constructor is fine for this class. 
+
+   public insert(int data){
+      
+     
+
+
+   }
+
+
+}
+
+class Node {
+
+
+  /******************
+   * CLASS FIELD(S) *
+   ******************/
+   int data;
+   Node prev;
+   Node next;
+
+ /************************
+  * CLASS CONSTRUCTOR(S) *
+  ************************/
+  // Default constructor is fine for this case.   
+
+}

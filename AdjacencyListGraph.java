16565fb95688f16f4d13b7fde8218feb31c204e3
Joseph Gormley <josephbgormley@gmail.com>
2018-03-26 20:20:54 -0400

    Added functionality to CircularLinkedList and AdjacencyListGraph.

--- AdjacencyListGraph.java
+++ AdjacencyListGraph.java
@@ -1,42 +1,61 @@
 /* Author: Joseph Gormley
    Date: March 22nd, 2018 */
+
+import java.util.LinkedList; 
+
 public class AdjacencyListGraph {
    
   /******************
    * CLASS FIELD(S) *
    ******************/
-   LinkedList<Integer>[] graph; 
-   
+   CircularLinkedList[] graph; 
+   int vertices; 
+
   /************************
    * CLASS CONSTRUCTOR(S) *
    ************************/
    public AdjacencyListGraph(int vertices){
-      graph = LinkedList[vertices];
+      graph = new CircularLinkedList[vertices];
+      this.vertices = vertices; 
    }
 
   /*******************
    * CLASS METHOD(S) *
    *******************/
+   public void initialize(){
+    
+      // Fill graph with empty Circular Linked Lists. 
+      for(int i = 0; i < vertices; i++){
+         graph[i] = new CircularLinkedList();
+      }
+   }
+
    /* Adds an edge to the matrix representation of the graph.
       vertex1 -> Start connecting point of the edge.
       vertex2 -> End connecting point of the edge. */
    public void addEdge(int vertex1, int vertex2){
+
+      System.out.println("Inserting edge " + vertex2 + " in graph[" + vertex1 + "]");
    
       // Add given edge to the graph representation. 
-      graph[vertex].insert(vertex2);
+      graph[vertex1].insert(vertex2);
+      graph[vertex1].printList();
       return; 
-     
    }
  
   /********************
    * HELPER METHOD(S) *
-   ********************
+   ********************/
    public void dfs(int vertex){
    
-      Stack<Integer> s = new Stack();
+      //Stack<Integer> s = new Stack<Integer>();
        
-      s.pop(vertex);
+      //s.pop(vertex);
        
+   }
+
+   public void printEdges(){
+   
    }
 
 }
--- CircularLinkedList.java
+++ CircularLinkedList.java
@@ -1,43 +1,90 @@
 /* Author: Joseph Gormle
    Date: March 26, 2018 */
 
-public class DoublyLinkedList {
+public class CircularLinkedList {
 
 
   /******************
    * CLASS FIELD(S) *
    ******************/
    int size;
-   Node front; 
+   Node rear; 
 
   /************************
    * CLASS CONSTRUCTOR(S) *
    ************************/ 
    // Default constructor is fine for this class. 
 
-   public insert(int data){
-      
-     
 
+  /*******************
+   * CLASS METHOD(S) *
+   *******************/
+   public Node insertInside(int data){
+      
+      // Create new node for data.
+      Node n = new Node(data);
+
+      // Edge case - empty list. 
+      if(rear == null){
+         System.out.println("insertInside() - Empty Case");
+         n.next = n;
+         return n;
+      }
+
+      Node front;
+      // Add to the end of the list.
+      front = rear.next; 
+      rear.next = n;
+      n.next = front;  
+      rear = rear.next;
+      size++;
+ 
+      return null;
+   }
+
+  /********************
+   * HELPER METHOD(S) *
+   ********************/
+   public void printList(){
+   
+      // Edge case - empty list.
+      if(rear == null){
+         System.out.println("printList() - Empty Case");
+         return;
+      } 
+
+      Node curr = rear.next;
+      do{
+         System.out.println(curr.data + "->");
+         curr = curr.next;
+      }while(curr != rear.next);
 
    }
 
+  /******************
+   * WRAPPER METHOD *
+   ******************/
+   public void insert(int data){ rear = insertInside(data); }
+ 
 
+// End of class
 }
 
 class Node {
-
 
   /******************
    * CLASS FIELD(S) *
    ******************/
    int data;
-   Node prev;
    Node next;
 
  /************************
   * CLASS CONSTRUCTOR(S) *
   ************************/
-  // Default constructor is fine for this case.   
+  public Node(int data){
+     this.data = data;
+     this.next = null;
+  }
 
+// End of class.
 }

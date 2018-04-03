73861ed8a08418255cacafb76bd53747cdce23bf
Joseph Gormley <josephbgormley@gmail.com>
2018-04-03 16:49:25 -0400

    Added NodeGen class to Queue.java.

--- Queue.java
+++ Queue.java
@@ -3,13 +3,13 @@ public class Queue<T> {
   /****************** 
    * CLASS FIELD(S) *
    ******************/
-   Node<T> front;
+   NodeGen<T> front;
    
   /************************
    * CLASS CONSTRUCTOR(S) *
    ************************/
    // Default constructor is fine for this class. 
-    
+     
   /*******************
    * CLASS METHOD(S) *
    *******************/
@@ -17,7 +17,7 @@ public class Queue<T> {
    public void enQueue(T data){
 
       // Create new node. 
-      Node<T> n = new Node<T>(data, null);
+      NodeGen<T> n = new NodeGen<T>(data);
  
       // Edge case, empty list.
       if(front == null){
@@ -26,7 +26,7 @@ public class Queue<T> {
       }
       
       // Traverse to the end of the queue. 
-      Node<T> curr = front;
+      NodeGen<T> curr = front;
       while(curr.next != null){
          curr = curr.next;         
       }
@@ -39,7 +39,7 @@ public class Queue<T> {
    /* Removes an item from the queue */
    public T deQueue(){
 
-      Node<T> ret = front;
+      NodeGen<T> ret = front;
       front = front.next;
 
       return ret.data;
@@ -54,9 +54,28 @@ public class Queue<T> {
    * HELPER METHOD(S) *
    ********************/
    public void printQueue(){
-      for(Node<T> curr = front; curr != null; curr = curr.next){
+      for(NodeGen<T> curr = front; curr != null; curr = curr.next){
          System.out.println(curr.data);
       }
    }
 
 } // End of class.
+
+class NodeGen<T> {
+
+  /*******************
+   * CLASS MEMBER(S) *
+   *******************/
+   T data;
+   NodeGen<T> next;
+
+  /************************
+   * CLASS CONSTRUCTOR(S) *
+   ************************/
+   public NodeGen(T data){
+      this.data = data;
+      this.next = null;
+   }
+   
+}
+

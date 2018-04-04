b662cf1b69562e98e220b80ac458b51d49f74947
Joseph Gormley <josephbgormley@gmail.com>
2018-04-04 16:24:44 -0400

    Added boilerplate code for Heap data structure.

---
+++ Heap.java
@@ -0,0 +1,33 @@
+public class Heap {
+
+    public static void main(String[] args){
+
+       /******************
+        * CLASS FIELD(S) *
+        ******************/
+
+       /************************
+        * CLASS CONSTRUCTOR(S) *
+        ************************/
+
+
+   }
+
+}
+
+
+class TreeNodeGen<T>{
+
+   /******************
+    * CLASS FIELD(S) *
+    ******************/
+    T data;
+    TreeNodeGen<T> leftChild;
+    TreeNodeGen<T> rightChild;
+
+   /************************
+    * CLASS CONSTRUCTOR(S) *
+    ************************/
+    // Default constructor is fine for this class.
+
+}
